44e9ef3f1449221c440ad2ec68996f527b68bb8c
AsmaaAkhtar <113789786+AsmaaAkhtar@users.noreply.github.com>
2023-04-11 22:59:15 +0500

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,48 @@
+/* console.log("Asma");
+let x=5 + "5";
+data types are almost same in every language 
+
+1-numbber
+2-String
+in js declare kiya hua datatype chnag ekr skty ho
+means ik variabcle mn jitna marzi chang kr lo it just dont  */
+
+/* x=5;
+x="Asma"; */
+/* 
+// In js it is allowed k ap a\ik variable ka datatype bar bar change rk skty ho 
+and it is called DYNAMIC VARIABLE */
+
+//String
+/* let x="Asma";
+x='My name is Asma AKhtar nd my father Name is M.Akhtar'; */
+
+
+//Boolean operator comaprsion k liye ye hamesh true ya false hota es k illawa kch nhi bta skta
+
+/* let x=2<3;
+console.log(x); */
+
+//object mn b multiple data store hota and array mn b
+/* let man = {name:"Asma",
+age: 21 };
+console.log(man); */
+
+/* Undefined is a kind of datatype jb ap ik avriable declare kr dety ho or us mn kch 
+store nhi kraty vo undefine data type hota hy */
+/* let x;
+console.log(x); */
+
+// or jb hum kch ="" lgyn gy tw us ka mtlb chnge ho ajye ga tw vo undefine nhi rahy ga
+
+
+/* let x="";
+console.log(x); */
+
+
+
+// #type of is kind of a function in js 
+//   tells about the datatype of the variable
+
+let x=typeof "asma";
+console.log(x);
